68ddd3201068c88589ef6adc2aebc8c0584f1353
Guillaume Smet <guillaume.smet@gmail.com>
2025-02-20 16:40:20 +0100

    Switch configuration to @ConfigMapping (#285)
    We plan to retire the legacy config classes at some point in the future
    so let's move to @ConfigMapping.
    
    Note: I also renamed quarkus.log.sentry to quarkus.log.sentry.enabled as
    the former is quite painful with YAML configuration.
    I kept quarkus.log.sentry as deprecated and there's a compatibility
    layer to avoid breaking older applications.

--- runtime/src/main/java/io/quarkus/logging/sentry/SentryProxyConfig.java
+++ runtime/src/main/java/io/quarkus/logging/sentry/SentryProxyConfig.java
@@ -3,38 +3,46 @@ package io.quarkus.logging.sentry;
 import java.util.Optional;
 
 import io.quarkus.runtime.annotations.ConfigGroup;
-import io.quarkus.runtime.annotations.ConfigItem;
+import io.smallrye.config.WithDefault;
+import io.smallrye.config.WithParentName;
 
 @ConfigGroup
-public class SentryProxyConfig {
+public interface SentryProxyConfig {
+
+    /**
+     * Determine whether to enable a Proxy for all Sentry outbound requests. This is also used for HTTPS
+     * requests.
+     *
+     * @deprecated we try to stay away from this pattern now, replace with {@code quarkus.log.sentry.enabled}.
+     */
+    @Deprecated(forRemoval = true)
+    @WithParentName
+    Optional<Boolean> enable();
+
     /**
      * Determine whether to enable a Proxy for all Sentry outbound requests. This is also used for HTTPS
      * requests.
      */
-    @ConfigItem(name = ConfigItem.PARENT)
-    public boolean enable;
+    @WithDefault("false")
+    boolean enabled();
 
     /**
      * Sets the host name of the proxy server.
      */
-    @ConfigItem
-    public Optional<String> host;
+    public Optional<String> host();
 
     /**
      * Sets the port number of the proxy server
      */
-    @ConfigItem
-    public Optional<Integer> port;
+    public Optional<Integer> port();
 
     /**
      * Sets the username to authenticate on the proxy server
      */
-    @ConfigItem
-    public Optional<String> username;
+    public Optional<String> username();
 
     /**
      * Sets the password to authenticate on the proxy server
      */
-    @ConfigItem
-    public Optional<String> password;
+    public Optional<String> password();
 }
